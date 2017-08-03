fb82153c01d33bc04a9fc4aa054ad4f8a491d4d9
phwebmachine <phwebmachine@gmail.com>
2017-08-04 00:41:57 +0800

    feat: Improve correct model detection rate
     - Increase voxel amount by 3 times
     - Remove table edge by increasing axis_min of x filter
     - Add noise reduction code
     - Increase RANSAC max_distance by 3 times to 0.003
     - Adjust ClusterTolerance, MinClusterSize, and MaxClusterSize
     - Include binaries for model.sav and training sets
     - Create object recognition.py that can be copy pasted to VM

--- pr2_robot/scripts/object_recognition.py
+++ pr2_robot/scripts/object_recognition.py
@@ -5,89 +5,50 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import LabelEncoder
 import pickle
+from sensor_stick.srv import GetNormals
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from visualization_msgs.msg import Marker
+from sensor_stick.marker_tools import *
+from sensor_stick.msg import DetectedObjectsArray
+from sensor_stick.msg import DetectedObject
+# from sensor_stick.pcl_helper import *
+from pcl_helper import *
 
-# from pcl_helper import get_color_list, ros_to_pcl, XYZRGB_to_XYZ, rgb_to_float
+import rospy
+import tf
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Float64
+from std_msgs.msg import Int32
+from std_msgs.msg import String
+from pr2_robot.srv import *
+from rospy_message_converter import message_converter
+import yaml
 
-# TODO remove in production
-import pcl
-from random import randint
-import struct
 
-DEV_FLAG = 1
+DEV_FLAG = 0
 OUTPUT_PCD_DIRECTORY = "output_pcd_files"
 
+# Helper function to get surface normals
+def get_normals(cloud):
+    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
+    return get_normals_prox(cloud).cluster
 
-def random_color_gen():
-    """ Generates a random color
+# Helper function to create a yaml friendly dictionary from ROS messages
+def make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose):
+    yaml_dict = {}
+    yaml_dict["test_scene_num"] = test_scene_num.data
+    yaml_dict["arm_name"]  = arm_name.data
+    yaml_dict["object_name"] = object_name.data
+    yaml_dict["pick_pose"] = message_converter.convert_ros_message_to_dictionary(pick_pose)
+    yaml_dict["place_pose"] = message_converter.convert_ros_message_to_dictionary(place_pose)
+    return yaml_dict
 
-        Args: None
-
-        Returns:
-            list: 3 elements, R, G, and B
-    """
-    r = randint(0, 255)
-    g = randint(0, 255)
-    b = randint(0, 255)
-    return [r, g, b]
-
-
-def get_color_list(cluster_count):
-    """ Returns a list of randomized colors
-
-        Args:
-            cluster_count (int): Number of random colors to generate
-
-        Returns:
-            (list): List containing 3-element color lists
-    """
-    if (cluster_count > len(get_color_list.color_list)):
-        for i in xrange(len(get_color_list.color_list), cluster_count):
-            get_color_list.color_list.append(random_color_gen())
-    return get_color_list.color_list
-
-
-def XYZRGB_to_XYZ(XYZRGB_cloud):
-    """ Converts a PCL XYZRGB point cloud to an XYZ point cloud (removes color info)
-
-        Args:
-            XYZRGB_cloud (PointCloud_PointXYZRGB): A PCL XYZRGB point cloud
-
-        Returns:
-            PointCloud_PointXYZ: A PCL XYZ point cloud
-    """
-    XYZ_cloud = pcl.PointCloud()
-    points_list = []
-
-    for data in XYZRGB_cloud:
-        points_list.append([data[0], data[1], data[2]])
-
-    XYZ_cloud.from_list(points_list)
-    return XYZ_cloud
-
-
-def rgb_to_float(color):
-    """ Converts an RGB list to the packed float format used by PCL
-
-        From the PCL docs:
-        "Due to historical reasons (PCL was first developed as a ROS package),
-         the RGB information is packed into an integer and casted to a float"
-
-        Args:
-            color (list): 3-element list of integers [0-255,0-255,0-255]
-
-        Returns:
-            float_rgb: RGB value packed as a float
-    """
-    hex_r = (0xff & color[0]) << 16
-    hex_g = (0xff & color[1]) << 8
-    hex_b = (0xff & color[2])
-
-    hex_rgb = hex_r | hex_g | hex_b
-
-    float_rgb = struct.unpack('f', struct.pack('i', hex_rgb))[0]
-
-    return float_rgb
-
+# Helper function to output to yaml file
+def send_to_yaml(yaml_filename, dict_list):
+    data_dict = {"object_list": dict_list}
+    with open(yaml_filename, 'w') as outfile:
+        yaml.dump(data_dict, outfile, default_flow_style=False)
 
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
@@ -97,8 +58,7 @@ def pcl_callback(pcl_msg):
         cloud = pcl_msg
     else:
         # TODO uncomment in production
-        # cloud = ros_to_pcl(pcl_msg)
-        pass
+        cloud = ros_to_pcl(pcl_msg)
 
     # Voxel Grid Downsampling
     vox = cloud.make_voxel_grid_filter()
@@ -110,7 +70,7 @@ def pcl_callback(pcl_msg):
     # Call the filter function to obtain the resultant downsampled point cloud
     cloud_filtered = vox.filter()
 
-    pcl.save(cloud_filtered, OUTPUT_PCD_DIRECTORY + "/voxel_downsampled.pcd")
+    # pcl.save(cloud_filtered, OUTPUT_PCD_DIRECTORY + "/voxel_downsampled.pcd")
     print("voxel downsampled cloud saved")
 
     # PassThrough Filter for z axis to remove table
@@ -138,8 +98,10 @@ def pcl_callback(pcl_msg):
 
     cloud_filtered = passthrough_x.filter()
 
-    pcl.save(cloud_filtered, OUTPUT_PCD_DIRECTORY + "/passthrough_filtered.pcd")
+    # pcl.save(cloud_filtered, OUTPUT_PCD_DIRECTORY + "/passthrough_filtered.pcd")
     print("passthrough filtered cloud saved")
+
+
 
     # remove noise from the sample
     outlier_filter = cloud_filtered.make_statistical_outlier_filter()
@@ -152,8 +114,8 @@ def pcl_callback(pcl_msg):
 
     # Finally call the filter function for magic
     cloud_filtered = outlier_filter.filter()
-    pcl.save(cloud_filtered, OUTPUT_PCD_DIRECTORY + "/noise_reduced.pcd")
-    print ("noise reduced cloud saved")
+    print "noise reduced"
+
 
     # RANSAC Plane Segmentation
     seg = cloud_filtered.make_segmenter()
@@ -177,8 +139,8 @@ def pcl_callback(pcl_msg):
     # Extract outliers - models that do not fit the model (non-planes)
     cloud_objects = cloud_filtered.extract(inliers, negative=True)
 
-    pcl.save(cloud_table, OUTPUT_PCD_DIRECTORY + "/cloud_table.pcd")
-    pcl.save(cloud_objects, OUTPUT_PCD_DIRECTORY + "/cloud_objects.pcd")
+    # pcl.save(cloud_table, OUTPUT_PCD_DIRECTORY + "/cloud_table.pcd")
+    # pcl.save(cloud_objects, OUTPUT_PCD_DIRECTORY + "/cloud_objects.pcd")
     print("RANSAC clouds saved")
 
     # Euclidean Clustering
@@ -215,61 +177,60 @@ def pcl_callback(pcl_msg):
     cluster_cloud = pcl.PointCloud_PointXYZRGB()
     cluster_cloud.from_list(color_cluster_point_list)
 
-    pcl.save(cluster_cloud, OUTPUT_PCD_DIRECTORY + "/cluster_cloud.pcd")
+    # pcl.save(cluster_cloud, OUTPUT_PCD_DIRECTORY + "/cluster_cloud.pcd")
 
     # TODO: Convert PCL data to ROS messages
-    # ros_cloud_objects = pcl_to_ros(cloud_objects)
-    # ros_cloud_objects = pcl_to_ros(cluster_cloud)
-    # ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cloud_objects = pcl_to_ros(cluster_cloud)
+    ros_cloud_table = pcl_to_ros(cloud_table)
 
     # TODO: Publish ROS messages
-    # pcl_objects_pub.publish(ros_cloud_objects)
-    # pcl_table_pub.publish(ros_cloud_table)
+    pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_table_pub.publish(ros_cloud_table)
 
 # Exercise-3 TODOs: identify the objects
 
-    # detected_objects_labels = []
-    # detected_objects = []
-    #
+    detected_objects_labels = []
+    detected_objects = []
+
     for index, pts_list in enumerate(cluster_indices):
         # Grab the points for the cluster
         pcl_cluster = cloud_objects.extract(pts_list)
         # TODO: convert the cluster from pcl to ROS using helper function
-        # ros_cluster = pcl_to_ros(pcl_cluster)
-        if index == 0:
-            pcl.save(pcl_cluster, OUTPUT_PCD_DIRECTORY + "/sample_cluster.pcd")
-    #
-    #     # Extract histogram features
-    #     # TODO: complete this step just as you did before in capture_features.py
-    #     chists = compute_color_histograms(ros_cluster, using_hsv=True)
-    #     normals = get_normals(ros_cluster)
-    #     nhists = compute_normal_histograms(normals)
-    #     feature = np.concatenate((chists, nhists))
-    #
-    #     # Make the prediction, retrieve the label for the result
-    #     # and add it to detected_objects_labels list
-    #
-    #     prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
-    #     label = encoder.inverse_transform(prediction)[0]
-    #     detected_objects_labels.append(label)
-    #
-    #     # Publish a label into RViz
-    #     label_pos = list(white_cloud[pts_list[0]])
-    #     label_pos[2] += .4
-    #     print(type(make_label))
-    #     print("label", label)
-    #     print("label pos",label_pos)
-    #     print("index",index)
-    #     print(make_label(label,label_pos, index))
-    #     object_markers_pub.publish(make_label(label,label_pos, index))
-    #
-    #     # Add the detected object to the list of detected objects.
-    #     do = DetectedObject()
-    #     do.label = label
-    #     do.cloud = ros_cluster
-    #     detected_objects.append(do)
+        ros_cluster = pcl_to_ros(pcl_cluster)
+
+        # Extract histogram features
+        # TODO: complete this step just as you did before in capture_features.py
+        chists = compute_color_histograms(ros_cluster, using_hsv=True)
+        normals = get_normals(ros_cluster)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
+
+        # Make the prediction, retrieve the label for the result
+        # and add it to detected_objects_labels list
+
+        prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
+        label = encoder.inverse_transform(prediction)[0]
+        detected_objects_labels.append(label)
+
+        # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        print(type(make_label))
+        print("label", label)
+        print("label pos",label_pos)
+        print("index",index)
+        print(make_label(label,label_pos, index))
+        object_markers_pub.publish(make_label(label,label_pos, index))
+
+        # Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
 
     # Publish the list of detected objects
+    rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
+    detected_objects_pub.publish(detected_objects)
 
     # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
     # Could add some logic to determine whether or not your object detections are robust
@@ -281,8 +242,29 @@ def pcl_callback(pcl_msg):
 
 
 if __name__ == '__main__':
-    cloud = pcl.load_XYZRGB('sample_pcd_files/3_objects.pcd')
+    # TODO: ROS node initialization
+    rospy.init_node('clustering', anonymous=True)
 
+    # TODO: Create Subscribers
+    pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size=1)
+
+    # TODO: Create Publishers
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", PointCloud2, queue_size=1)
+
+    # Initialize color_list
     get_color_list.color_list = []
 
-    pcl_callback(cloud)
+    # Load Model From disk
+    model = pickle.load(open('model3.sav', 'rb'))
+    clf = model['classifier']
+    encoder = LabelEncoder()
+    encoder.classes_ = model['classes']
+    scaler = model['scaler']
+
+    # TODO: Spin while node is not shutdown
+    while not rospy.is_shutdown():
+        rospy.spin()
